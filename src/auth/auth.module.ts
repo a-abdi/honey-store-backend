ef454ce9dc52bef61abf7f1f4e8141d64f1a7f21
a-badi <ali.abdi.1991@gmail.com>
2023-01-15 23:36:39 +0330

    review jwt-strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
-import 'dotenv/config'
+import 'dotenv/config';
 
 @Module({
   controllers: [AuthController],
