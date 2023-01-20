43b21e07145d103171b09dad97359c724a95fff3
a-badi <ali.abdi.1991@gmail.com>
2023-01-20 21:02:37 +0330

    create user entity

--- src/app/users/entities/user.entity.ts
+++ src/app/users/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema} from "mongoose";
+
+@Schema({ timestamps: true })
+export class User {
+    @Prop()
+    _id: MongooseSchema.Types.ObjectId;
+
+    @Prop()
+    firstName: string;
+
+    @Prop()
+    lastName: string;
+
+    @Prop()
+    phoneNumber: string;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    postalCode: string;
+
+    @Prop()
+    city: string;
+
+    @Prop()
+    password: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
