650dad8326b88cb40693543a8f626dcd3afb8aa6
a-badi <ali.abdi.1991@gmail.com>
2023-01-21 12:10:07 +0330

    create category entity

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,17 @@
-export class Category {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema } from "mongoose";
+
+@Schema({timestamps: true})
+export class Category {
+    _id: MongooseSchema.Types.ObjectId;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    description: string;
+}
+
+export type CategoryDocument = Category & Document;
+
+export const CategoryModel = SchemaFactory.createForClass(Category);
