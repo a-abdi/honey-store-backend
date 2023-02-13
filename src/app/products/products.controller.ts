76045066b6181a2f868f09ab4ce300daed07ad1a
a-badi <ali.abdi.1991@gmail.com>
2023-02-14 00:13:19 +0330

    review auth for product

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -45,6 +45,8 @@ export class ProductsController {
     return this.productsService.findOne(params._id);
   }
 
+  @Roles(Role.Admin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
   update(@Param() params: MongoIdParams, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(params._id, updateProductDto);
