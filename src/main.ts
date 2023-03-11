0ae32b4c3b5827f48beff18bdcfc96154ad93800
a-badi <ali.abdi.1991@gmail.com>
2023-03-11 23:43:48 +0330

    add NestExpressApplication interface to app and create static route for upload/file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { useContainer } from 'class-validator';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   useContainer(app.select(AppModule), {fallbackOnErrors: true}); 
+  app.useStaticAssets(join(__dirname, '..', 'upload'), {
+    index: false,
+    prefix: '/upload',
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     // forbidNonWhitelisted: true,
