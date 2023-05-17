b187187c4b14eda7a5e2ab390cbda07033b5722b
a-badi <ali.abdi.1991@gmail.com>
2023-05-17 18:42:35 +0330

    implement property entity

--- src/app/property/entities/property.entity.ts
+++ src/app/property/entities/property.entity.ts
@@ -1,1 +1,24 @@
-export class Property {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema} from "mongoose";
+import { Category } from "src/app/categories/entities/category.entity";
+
+@Schema({ timestamps: true })
+export class Property {
+    _id: MongooseSchema.Types.ObjectId;
+
+    @Prop({ required: true, unique: true })
+    label: string;
+
+    @Prop({ required: true})
+    type: string;
+
+    @Prop()
+    unit: string;
+
+    @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Category' })
+    category: MongooseSchema.Types.ObjectId | Category;
+}
+
+export type ProppertyDocument = Property & Document;
+
+export const PropertySchema = SchemaFactory.createForClass(Property);
