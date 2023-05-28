cdcebf708c8506e39904af857a6fa2327c7f6b92
a-badi <ali.abdi.1991@gmail.com>
2023-05-28 23:50:03 +0330

    implement dto for viewpoint

--- src/app/viewpoint/dto/create-viewpoint.dto.ts
+++ src/app/viewpoint/dto/create-viewpoint.dto.ts
@@ -1,1 +1,23 @@
-export class CreateViewpointDto {}
+import { IsMongoId, IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from "class-validator";
+import { Schema } from "mongoose";
+
+export class CreateViewpointDto {
+    @MaxLength(25)
+    @IsOptional()
+    @IsString()
+    title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    text: string;
+
+    @Min(1)
+    @Max(5)
+    @IsOptional()
+    @IsNumber()
+    score: number;
+
+    @IsNotEmpty()
+    @IsMongoId()
+    productId: Schema.Types.ObjectId;
+}
