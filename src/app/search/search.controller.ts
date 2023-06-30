f6f7aa5624b7ab280b22a5c2b1d2d530cacc6c6b
a-badi <ali.abdi.1991@gmail.com>
2023-06-30 21:49:22 +0330

    fix bug search value is null

--- src/app/search/search.controller.ts
+++ src/app/search/search.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { SearchDto } from './dto/product-query.dto';
 import { ProductsService } from '../products/products.service';
+import { Product } from '../products/entities/product.entity';
+import { UrlHelper } from 'src/common/helper/url.helper';
+import { Request } from 'express';
 
 @Controller('search')
 export class SearchController {
-  constructor(private readonly productService: ProductsService) {}
+  constructor(
+    private readonly productService: ProductsService,
+    private readonly urlHelper: UrlHelper,
+  ) {}
 
   @Get()
-  async search(@Query() { q }: SearchDto) {
-    
-    return await this.productService.search(q);
+  async search(@Query() { q }: SearchDto, @Req() request: Request) {
+    let products: Product[] = [];
+    if (q) {
+      products = await this.productService.search(q);
+    } else {
+      products = await this.productService.findAll({});
+    }
+    products.map(product => {
+      this.urlHelper.bindHostUrlToProduct(product, request);
+    });
+    return products;
   }
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { ProductsModule } from '../products/products.module';
+import { UrlHelper } from 'src/common/helper/url.helper';
 
 @Module({
   controllers: [SearchController],
+  providers: [UrlHelper],
   imports: [ProductsModule]
 })
 export class SearchModule {}
