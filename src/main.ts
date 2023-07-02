b66b9e3726bd43a6c1a5c50f248344c7a623cc8f
a-badi <ali.abdi.1991@gmail.com>
2023-07-02 14:41:20 +0330

    add swagger in nestjs

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,19 @@ import { AppModule } from './app/app.module';
 import { useContainer } from 'class-validator';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Honey Backend Store')
+    .setDescription('Honey Backend Store')
+    .setVersion('1.0')
+    .addTag('Honey')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   useContainer(app.select(AppModule), {fallbackOnErrors: true}); 
   app.useStaticAssets(join(__dirname, '..', 'upload'), {
     index: false,
