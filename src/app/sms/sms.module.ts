f35c94a3130c239b2eadc23565c264586140f9a3
a-badi <ali.abdi.1991@gmail.com>
2023-08-03 02:25:03 +0330

    send verify sms

--- src/app/sms/sms.module.ts
+++ src/app/sms/sms.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  providers: [SmsService]
+  providers: [SmsService],
+  imports: [HttpModule],
+  exports: [SmsService]
 })
 export class SmsModule {}
--- src/app/sms/sms.service.ts
+++ src/app/sms/sms.service.ts
@@ -1,4 +1,43 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { catchError, firstValueFrom, map } from 'rxjs';
+import { CreateTransactionInterFace } from '../orders/interface/interface';
+import { AxiosError } from 'axios';
+import { ParametersInterface } from './interface/parameter.interface';
 
 @Injectable()
-export class SmsService {}
+export class SmsService {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly httpService: HttpService,
+    ) {}
+
+    async sendVerifyCode(mobile: string, parameters: ParametersInterface[], templateId: number = 100000) {
+        const data = JSON.stringify({
+            mobile,
+            templateId,
+            parameters
+        });
+        const url = this.configService.get<string>('SMS_VERIFY_URL');
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'text/plain',
+            'x-api-key': this.configService.get<string>('SMS_X_API_KEY')
+        };
+
+        return await firstValueFrom(
+            this.httpService.post<CreateTransactionInterFace>(url, data, { headers }).pipe(map((res) => res.data)).pipe(
+                catchError(async (error: AxiosError) => {
+                    console.log(error);
+                    
+                    // this.productHelper.increaseProductQuantity(order.cart);
+                    // this.ordersTransactionsService.updateOrder(orderId, { "transaction.error": error.response?.data || error.message });
+                    // throw new InternalServerErrorException(Message.ERROR_OCCURRED());
+                }),
+            ),
+        );
+
+
+    }
+}
